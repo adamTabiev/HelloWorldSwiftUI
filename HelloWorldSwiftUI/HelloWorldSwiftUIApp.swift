0d2795f61956fa688290e90a55706ba1dcd5db70
Adam Tabiev <tabievadam@gmail.com>
2022-04-18 11:37:49 +0300

    Initial Commit

---
+++ HelloWorldSwiftUI/HelloWorldSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldSwiftUIApp.swift
+//  HelloWorldSwiftUI
+//
+//  Created by Адам Табиев on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
